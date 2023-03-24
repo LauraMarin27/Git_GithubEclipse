ddbb88972de5d721062c3bbde088963a0d6afa78
Laura Marin <lalamarin0927@gmail.com>
2023-03-24 14:26:56 +0100

    Integrantes del grupo

--- Git_GitHub en Eclipse/src/GitenEclipse.java
+++ Git_GitHub en Eclipse/src/GitenEclipse.java
@@ -4,6 +4,13 @@ public class GitenEclipse {
 	public static void main(String[] args) {
 		System.out.println("Hola , estamos aprendiendo a usar Git y GitHub en Eclipse");
 		System.out.println("Clase de Entornos de Desarrollo");
+		String[] nombres = {"Javier", "Miriam", "Ryan", "Laura"};
+        
+        System.out.println("Los integrantes del grupo son:");
+        
+        for (String nombre : nombres) {
+            System.out.println(nombre);
+        }
 		
 
 	}
